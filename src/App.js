bb8e0e450cd65580f0a09e597836f035ab1a4b46
unknown <khahlem91@gmail.com>
2021-05-19 22:25:58 +0100

    first commit'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Person: {
+        fullName: "AhlemKhalili",
+        bio: " Ahlem Khalili ingénieur en informatique industriel diplomée de l'Enet'Com depuis 2017 et maintenant une élève à GOMYCODE.",
+        imgSrc: "https://thumbs.dreamstime.com/z/profil-de-fille-avec-des-fleurs-91156883.jpg",
+        profession: "Ingénieur en Informatique Industriel",
+        shows: true,
+
+      }
+    }
+
+  };
+
+  
+  handleShowPerson = () => {
+    
+    this.setState({
+      shows: !this.state.shows,
+      
+    });
+    console.log(this.state.shows)
+  };
+
+
+
+  render() {
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', margintop: 50 }}  >
+        <button style={{  color: 'black', backgroundColor: '#ff6f6f', fontSize: '30px', padding: '30px 70px' }} 
+        onClick={this.handleShowPerson}>{!this.state.shows ? 'Show' : 'Hide'}</button>
+        {this.state.shows ?
+        
+          <div style={{  margin: 25, padding: 15 }}>
+            
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around' }}>
+              <h1>{this.state.Person.fullName}</h1>
+
+              <img style={{ width: 200, height: 200, }} src={this.state.Person.imgSrc}></img>
+            </div>
+            <div style={{ margin: 25 }}>
+              <h3>{this.state.Person.bio}</h3>
+              <h2>{this.state.Person.profession}</h2>
+            </div>
+
+            
+          </div>
+          :<h1> Hi, you can press to the buttun please</h1>
+          
+        }
+
+      </div>
+    )
+  }
+  
 }
 
-export default App;
+export default App
